b71bde46ee623d8a1556fb6855252d10ad727bad
Paulo andrade <source.compu@gmail.com>
2018-02-05 09:48:53 -0600

    Se creo politica de privacidad, terminos de condiciones de uso y servicio y el footer de la app.

--- src/app/modules/init/init-routing.module.ts
+++ src/app/modules/init/init-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InitComponent } from './../../components/init/init.component';
+import { PrivacidadComponent } from './../../components/privacidad/privacidad.component';
+import { CondicionesComponent } from './../../components/condiciones/condiciones.component';
+import { FooterComponent } from './../../components/footer/footer.component';
 
 const routes: Routes = [
-  { path: '', component: InitComponent }
+  { path: '', component: InitComponent },
+  { path: 'politica-de-privacidad', component: PrivacidadComponent },
+  { path: 'terminos-y-condiciones-de-uso', component: CondicionesComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [InitComponent]
+  declarations: [
+  	InitComponent,
+  	PrivacidadComponent,
+  	CondicionesComponent,
+    FooterComponent
+  ]
 })
 export class InitRoutingModule { }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -16,15 +16,20 @@ export class ChatService {
 	userDoc: AngularFirestoreDocument<UserModel>;
 
 	constructor(public afs : AngularFirestore) {
-	    this.chatsCollection = this.afs.collection('chats');
+	    this.chatsCollection = this.afs.collection('chats', ref => {
+	    	// Limitamos la salida a solo los ultimos 30 items
+	    	return ref.orderBy('date', 'desc').limit(30);
+	    });
+	    // Obtenemos cada uno de los documentos
 	    this.chats = this.chatsCollection.snapshotChanges().map(changes => {
 	      	return changes.map(_data => {
-	      		// Obtenemos cada uno de los documento
+	      		// Obtenemos cada uno de los documentos
 	        	const data = _data.payload.doc.data() as ChatModel;
 	        	data.id = _data.payload.doc.id;
 	        	// Obtenemos los datos de usuario
 	        	this.userDoc = this.afs.doc(`users/${data.uid}`);
 	        	const user = this.userDoc.valueChanges().subscribe(user => {
+	        		// Los asignamos al documento chat
 	        		data.name = user.displayName;
 	        		data.img = user.photoURL;
 	        	});
